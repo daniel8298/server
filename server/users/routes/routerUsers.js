cf8e3ad8f7f44d42b167a9b4af8732d775510f53
daniel8298 <daniel8298@gmail.com>
2023-08-28 16:45:27 +0300

    client

--- server/users/routes/routerUsers.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { createNewUser } = require("../controller/usersController");
-
-const userRouter = express.Router();
-
-userRouter.post("/registration/", createNewUser);
-
-module.exports = userRouter;
